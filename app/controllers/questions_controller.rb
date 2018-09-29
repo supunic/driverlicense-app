1d8a47696fe3751f6321be069c67079f090094b4
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-09-29 21:05:08 +0900

    insert img

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,8 +2,10 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
     @fields = Field.all
-    @temporary = @fields.where(temporary: true)
-    @main = @fields.where(temporary: false)
+    @temporary = @fields.where(id: 1..16)
+    @temporary_test = @fields.where(id: 17..19)
+    @main = @fields.where(id: 20..34)
+    @main_test = @fields.where(id: 35..36)
   end
 
   def devide
