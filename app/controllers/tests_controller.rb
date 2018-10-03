ef8424c5c77ef4368bfa7068cae91699d0ff5664
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-10-03 21:44:49 +0900

    env test2

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,6 @@ class TestsController < ApplicationController
   def letsencrypt
     if params[:id] == ENV["LETSENCRYPT_REQUEST"]
     @test = ENV["LETSENCRYPT_RESPONSE"]
-    binding.pry
     end
   end
 end
