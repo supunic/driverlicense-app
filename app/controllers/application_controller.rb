a73df4ea37f65bb436473dc6b2374ccba17f5cd6
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-10-04 20:28:31 +0900

    301 redirect2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :ensure_domain
+  before_action :ensure_domain
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
