455218b8c60c12ccf49017f041266e60971ec392
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-10-04 20:25:36 +0900

    301 redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
@@ -10,5 +11,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:sex])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:age])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  FQDN = 'driverlicense-app.supunic.com'
+  def ensure_domain
+   return unless /\.herokuapp.com/ =~ request.host
+   port = ":#{request.port}" unless [80, 443].include?(request.port)
+   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
   end
 end
