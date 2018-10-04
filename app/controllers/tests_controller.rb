218bc968057444f3afbf9d318ad82f9b285322c2
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-10-04 11:19:59 +0900

    SSL challenge

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,7 @@
 class TestsController < ApplicationController
   def letsencrypt
     if params[:id] == ENV["LETSENCRYPT_REQUEST"]
-    @test = ENV["LETSENCRYPT_RESPONSE"]
+    render plain: ENV["LETSENCRYPT_RESPONSE"]
     end
   end
 end
